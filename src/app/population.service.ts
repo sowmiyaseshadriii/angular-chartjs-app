b4957f6541048ba3bb8c910ddc27aeb0b78d4b21
sowmiyaseshadri <sowmiyaseshadrii@gmail.com>
2018-04-09 21:37:47 +0800

    first commit

--- src/app/population.service.ts
+++ src/app/population.service.ts
@@ -7,7 +7,7 @@ export class PopulationService {
   constructor(private _http: HttpClient) { }
   
     populationbyCountry() {
-      return this._http.get("http://api.population.io/1.0/population/2017/aged/99/")
+      return this._http.get("https://api.myjson.com/bins/t8csj")
         .map(result => result);
     }
 
